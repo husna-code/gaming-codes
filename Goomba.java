7fbf0c0f24047d1d33853d5c73d98c873114cfd3
husna-code <79115716+husna-code@users.noreply.github.com>
2021-02-15 16:51:08 -0500

    Create Goomba.java
    creating the goombas/enemies

---
+++ Goomba.java
@@ -0,0 +1,95 @@
+package com.brent.mario.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.utils.Array;
+import com.brent.mario.MarioBros;
+import com.brent.mario.Screens.PlayScreen;
+
+import java.util.Vector;
+
+public class Goomba extends Enemy
+{
+    private float stateTime;
+    private Animation <TextureRegion> walkAnimation;
+    private Array<TextureRegion> frames;
+    private boolean setToDestroy;
+    private boolean destroyed;
+
+    public Goomba(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+        frames = new Array<TextureRegion>();
+        for (int i = 0; i < 2; i++)
+            frames.add(new TextureRegion(screen.getAtlas().findRegion("goomba"),i*0,0,16,16));
+        walkAnimation = new Animation <TextureRegion>(0.4F, frames);
+        stateTime = 0;
+        setBounds(getX(), getY(), 16/MarioBros.PPM, 16/MarioBros.PPM);
+        setToDestroy = false;
+        destroyed = false;
+
+    }
+
+    public void update(float dt) {
+        stateTime += dt;
+        if (setToDestroy && !destroyed) {
+            world.destroyBody(b2body);
+            destroyed = true;
+            setRegion(new TextureRegion(screen.getAtlas().findRegion("goomba"), 32, 0, 16, 16));
+            stateTime = 0;
+        }
+        else if (!destroyed) {
+            setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+            setRegion((TextureRegion) walkAnimation.getKeyFrame(stateTime, true));
+        }
+    }
+
+    @Override
+    protected void defineEnemy() {
+        BodyDef bdef=new BodyDef();
+        bdef.position.set(32 /MarioBros.PPM,32 /MarioBros.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape=new PolygonShape();
+        shape.setAsBox(16/2 /MarioBros.PPM,16/2 /MarioBros.PPM);
+        fdef.filter.categoryBits = MarioBros.ENEMY_BIT;
+        fdef.filter.maskBits = MarioBros.GROUND_BIT | MarioBros.COIN_BIT | MarioBros.BRICK_BIT | MarioBros.ENEMY_BIT | MarioBros.OBJECT_BIT | MarioBros.MARIO_BIT | MarioBros.ENEMY_HEAD_BIT ;
+
+
+        fdef.shape=shape;
+        b2body.createFixture(fdef);
+
+        //create head
+        PolygonShape head = new PolygonShape();
+        Vector2[] vertice = new Vector2[4];
+        vertice[0] = new Vector2(-5, 8).scl(1/ MarioBros.PPM);
+        vertice[1] = new Vector2(5, 8).scl(1/ MarioBros.PPM);
+        vertice[2] = new Vector2(-3, 3).scl(1/ MarioBros.PPM);
+        vertice[3] = new Vector2(3, 3).scl(1/ MarioBros.PPM);
+        head.set(vertice);
+
+        fdef.shape = head;
+        fdef.restitution = 0.5f;
+        fdef.filter.categoryBits = MarioBros.ENEMY_HEAD_BIT;
+        b2body.createFixture(fdef).setUserData(this);
+
+    }
+
+    public void draw(Batch batch){
+        if(!destroyed || stateTime < 1)
+            super.draw(batch);
+    }
+
+    @Override
+    public void hitOnHead() {
+
+        setToDestroy = true;
+    }
+}
